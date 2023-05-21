95878538aeeb57aee18ba419e2436d59263bbd07
Khulood Alhamed <Khuloodalhamed@gmail.com>
2023-05-21 19:47:58 +0300

    Initial Commit

---
+++ SelfCheckIn/SelfCheckInApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfCheckInApp.swift
+//  SelfCheckIn
+//
+//  Created by Khulood Alhamed on 21/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SelfCheckInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
